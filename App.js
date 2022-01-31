63d9ddfc90215924ab7d7c314822b4284b3b9c22
Mia Huovinen <mia.huovinen@gmail.com>
2022-01-31 20:15:52 +0200

    Tehtävä 8: Osoite 0.1

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+
 
 export default function App() {
   return (
